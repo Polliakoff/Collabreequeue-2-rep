5699d527fe1b6a176e38be99a9038ec9e5d48dbd
Polyakov Daniil <70587587+Polliakoff@users.noreply.github.com>
2021-10-22 17:00:05 +0300

    test_ship_emansipated

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,9 +13,9 @@ MainWindow::MainWindow(QWidget *parent)
     {
         korablik.emplace_back(std::make_unique<ship_physics>(575,650,0,0));
     }
-///===========тестовый
-    korablik.emplace_back(std::make_unique<ship_physics>(575,650,0,0));
-///===========тестовый
+    ///===========тестовый
+    test_ship = std::make_unique<ship_physics>(575,650,0,0);
+    ///===========тестовый
     timer = std::make_unique<QTimer>();
 
     map = std::make_unique<pathway>();
@@ -52,15 +52,15 @@ void MainWindow::on_pushButton_clicked()
 {
     ui->graphicsView->setScene(scene.get());
 
-    int t = 60;
-    for(int i = 0; i < t; i++){      
+    int t = korablik.size();
+    for(int i = 0; i < t; i++){
         update_connections.emplace_back(connect(timer.get(), &QTimer::timeout,  [=](){korablik[i]->update(*map);}));
         think_n_do_connections.emplace_back(connect(timer.get(), &QTimer::timeout,  [=](){korablik[i]->think_n_do();}));
     }
-///===========тестовый
-    update_connections.emplace_back(connect(timer.get(), &QTimer::timeout,  [=](){korablik[t]->update(*map);}));
-    think_n_do_connections.emplace_back(connect(timer.get(), &QTimer::timeout,  [=](){korablik[t]->dumb_n_do(neuron1, neuron2, neuron3, neuron4);}));
-///===========тестовый
+    ///===========тестовый
+    test_update_connection = connect(timer.get(), &QTimer::timeout,  [=](){test_ship->update(*map);});
+    test_think_n_do_connection = connect(timer.get(), &QTimer::timeout,  [=](){test_ship->dumb_n_do(neuron1, neuron2, neuron3, neuron4);});
+    ///===========тестовый
     connect(timer.get(), SIGNAL(timeout()), this, SLOT(painter()));
     //disconnect(timer.get(), &QTimer::timeout,)
 
@@ -71,6 +71,7 @@ void MainWindow::on_pushButton_clicked()
 void MainWindow::painter()
 {
     scene->clear();
+    int ship_number = 0;
     for(auto shp = korablik.begin(); shp!=korablik.end(); ){
 
         qdraw_polygon(shp->get()->body,scene.get());
@@ -91,34 +92,62 @@ void MainWindow::painter()
         scene->addEllipse(shp->get()->point_seen[5].first-10,shp->get()->point_seen[5].second-10,20,20, QPen(Qt::lightGray));
 
 
-//        if(shp->get()->collided) {
-//            korablik.erase(shp);
-//            //korablik.shrink_to_fit();
-//        }
+        if(shp->get()->collided) {
+            korablik.erase(shp);
+            disconnect(update_connections[ship_number]);
+            update_connections.erase(update_connections.begin() + ship_number);
+            disconnect(think_n_do_connections[ship_number]);
+            think_n_do_connections.erase(think_n_do_connections.begin() + ship_number);
+            //korablik.shrink_to_fit();
+        }
         ++shp;
+        ++ship_number;
     }
 
-///===========тестовый
-    ui->lineEdit->setText(QString::number(korablik[60]->path.first));
-    ui->lineEdit_2->setText(QString::number(korablik[60]->path.second));
-    ui->lineEdit_3->setText(QString::number(korablik[60]->abs_velocity));
-    ui->lineEdit_4->setText(QString::number(korablik[60]->to_turn_to*180/M_PI));
-    ui->lineEdit_5->setText(QString::number(korablik[60]->velocity_projection));
-    ui->lineEdit_6->setText(QString::number(korablik[60]->velocity_x));
-    ui->lineEdit_7->setText(QString::number(korablik[60]->velocity_y));
-    ui->lineEdit_8->setText(QString::number(korablik[60]->eyes[2].direction[0]));
-    ui->lineEdit_9->setText(QString::number(korablik[60]->actual_angle));
-    ui->lineEdit_10->setText(QString::number(korablik[60]->fuel));
-    scene->addEllipse(korablik[60]->get_position().first-10,korablik[60]->get_position().second-10,20,20, QPen(Qt::red));
-    scene->addLine(korablik[60]->get_position().first,korablik[60]->get_position().second,
-            korablik[60]->get_position().first+korablik[60]->path.first,korablik[60]->get_position().second+korablik[60]->path.second, QPen(Qt::red));
-    scene->addLine(korablik[60]->get_position().first,korablik[60]->get_position().second,
-            korablik[60]->get_position().first+korablik[60]->velocity_x*50,korablik[60]->get_position().second+korablik[60]->velocity_y*50, QPen(Qt::blue));
-    scene->addLine(korablik[60]->get_position().first,korablik[60]->get_position().second,
-            korablik[60]->get_position().first+(korablik[60]->path.first*korablik[60]->velocity_projection/vector_module(korablik[60]->path.first,korablik[60]->path.second))*50,
-            korablik[60]->get_position().second+(korablik[60]->path.second*korablik[60]->velocity_projection/vector_module(korablik[60]->path.first,korablik[60]->path.second))*50,
-            QPen(Qt::green));
-///===========тестовый
+    ///===========тестовый
+    qdraw_polygon(test_ship->body,scene.get());
+    qdraw_polygon(*map,scene.get());
+
+    scene->addLine(test_ship->point_seen[0].first,test_ship->point_seen[0].second,
+            test_ship->point_seen[1].first,test_ship->point_seen[1].second, QPen(Qt::lightGray));
+    scene->addLine(test_ship->point_seen[2].first,test_ship->point_seen[2].second,
+            test_ship->point_seen[3].first,test_ship->point_seen[3].second, QPen(Qt::lightGray));
+    scene->addLine(test_ship->point_seen[4].first,test_ship->point_seen[4].second,
+            test_ship->point_seen[5].first,test_ship->point_seen[5].second, QPen(Qt::lightGray));
+
+    scene->addEllipse(test_ship->point_seen[0].first-10,test_ship->point_seen[0].second-10,20,20, QPen(Qt::lightGray));
+    scene->addEllipse(test_ship->point_seen[1].first-10,test_ship->point_seen[1].second-10,20,20, QPen(Qt::lightGray));
+    scene->addEllipse(test_ship->point_seen[2].first-10,test_ship->point_seen[2].second-10,20,20, QPen(Qt::lightGray));
+    scene->addEllipse(test_ship->point_seen[3].first-10,test_ship->point_seen[3].second-10,20,20, QPen(Qt::lightGray));
+    scene->addEllipse(test_ship->point_seen[4].first-10,test_ship->point_seen[4].second-10,20,20, QPen(Qt::lightGray));
+    scene->addEllipse(test_ship->point_seen[5].first-10,test_ship->point_seen[5].second-10,20,20, QPen(Qt::lightGray));
+
+    ui->lineEdit->setText(QString::number(test_ship->path.first));
+    ui->lineEdit_2->setText(QString::number(test_ship->path.second));
+    ui->lineEdit_3->setText(QString::number(test_ship->abs_velocity));
+    ui->lineEdit_4->setText(QString::number(test_ship->to_turn_to*180/M_PI));
+    ui->lineEdit_5->setText(QString::number(test_ship->velocity_projection));
+    ui->lineEdit_6->setText(QString::number(test_ship->velocity_x));
+    ui->lineEdit_7->setText(QString::number(test_ship->velocity_y));
+    ui->lineEdit_8->setText(QString::number(test_ship->eyes[2].direction[0]));
+    ui->lineEdit_9->setText(QString::number(test_ship->actual_angle));
+    ui->lineEdit_10->setText(QString::number(test_ship->fuel));
+    scene->addEllipse(test_ship->get_position().first-10,test_ship->get_position().second-10,20,20, QPen(Qt::red));
+    scene->addLine(test_ship->get_position().first,test_ship->get_position().second,
+                   test_ship->get_position().first+test_ship->path.first,test_ship->get_position().second+test_ship->path.second, QPen(Qt::red));
+    scene->addLine(test_ship->get_position().first,test_ship->get_position().second,
+                   test_ship->get_position().first+test_ship->velocity_x*50,test_ship->get_position().second+test_ship->velocity_y*50, QPen(Qt::blue));
+    scene->addLine(test_ship->get_position().first,test_ship->get_position().second,
+                   test_ship->get_position().first+(test_ship->path.first*test_ship->velocity_projection/vector_module(test_ship->path.first,test_ship->path.second))*50,
+                   test_ship->get_position().second+(test_ship->path.second*test_ship->velocity_projection/vector_module(test_ship->path.first,test_ship->path.second))*50,
+                   QPen(Qt::green));
+
+    if(test_ship->collided) {
+        test_ship = std::make_unique<ship_physics>(700,700,0,0);
+        disconnect(test_update_connection);
+        disconnect(test_think_n_do_connection);
+    }
+    ///===========тестовый
 }
 
 
